412caea7d686f7ad518d6da26abe81b0639fc3d9
ahmad amam <ahmadfatkhul.amam@gmail.com>
2020-11-23 20:52:01 +0700

    fixing register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,18 +1,18 @@
 const { user } = require('../models')
 const responseStandart = require('../helpers/response')
-const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const joi = require('joi')
 
 module.exports = {
     loginUser: async (req, res) => {
         const data = await user.findOne({ where: { telphone: req.body.phone } })
+        console.log(data)
         if (data !== null) {
             jwt.sign({ id: data.id }, process.env.APP_KEY, (err, token) => {
                 if (err) {
                     return responseStandart(res, 'Error', { error: err.message }, 500, false)
                 } else {
-                    return responseStandart(res, `Hello Readers ${data.id}`, { token })
+                    return responseStandart(res, `Hello moconers ${data.id}`, { token })
                 }
             })
         } else {
@@ -25,13 +25,22 @@ module.exports = {
         })
         let { value: results, error } = schema.validate(req.body)
         if (!error) {
-            const salt = bcrypt.genSaltSync(10)
-            const hashedPass = bcrypt.hashSync(results.phone, salt)
             const dataUser = {
                 telphone: results.phone,
             }
-            await user.create(dataUser)
-            return responseStandart(res, 'register success', {})
+            const data = await user.create(dataUser)
+            if (data !== null) {
+                jwt.sign({ id: data.id }, process.env.APP_KEY, (err, token) => {
+                    console.log(token);
+                    if (err) {
+                        return responseStandart(res, 'Error', { error: err.message }, 500, false)
+                    } else {
+                        return responseStandart(res, `Hello mocconers ${data.id}`, { token })
+                    }
+                })
+            } else {
+                return responseStandart(res, 'wrong email or password', {}, 400, false)
+            }
         } else {
             return responseStandart(res, 'error', {}, 401, false)
         }
